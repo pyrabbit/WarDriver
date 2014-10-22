30a51576d1335828897446abbf80eaf21014e27f
Matt <morahood@gmail.com>
2014-10-22 18:57:51 -0400

    small changes

--- src/com/blackgatetech/wardriver/lib/KismetClient.java
+++ src/com/blackgatetech/wardriver/lib/KismetClient.java
@@ -8,6 +8,8 @@ import java.io.OutputStreamWriter;
 import java.net.ConnectException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class KismetClient implements Runnable {
     // Declare variables used throughout connection
@@ -16,6 +18,10 @@ public class KismetClient implements Runnable {
     private BufferedReader fromServer;
     private BufferedWriter toServer;
     Socket socket;
+    
+    // Declare global variables for network statistics
+    Set<String> networksSeen = new HashSet<>();
+    private int networkCount = 0;
     
     public KismetClient() {
         this.port = 2501;
@@ -41,8 +47,8 @@ public class KismetClient implements Runnable {
             
             // Send Kismet commands 
             toServer.write("!0 REMOVE TIME\r\n");
-            toServer.write("!0 ENABLE BSSID bssid,channel\r\n");
-            toServer.write("!0 ENABLE SSID mac,ssid\r\n");
+            toServer.write("!0 ENABLE BSSID bssid,channel,type\r\n");
+//            toServer.write("!0 ENABLE SSID mac,ssid\r\n");
             
             // Flush the output stream
             toServer.flush();
@@ -75,12 +81,28 @@ public class KismetClient implements Runnable {
         try {
             while(true) {
                 // grab next kismet message
-                System.out.println(fromServer.readLine());
-                // parse kismet message
+                String kismetData = fromServer.readLine();
+                System.out.println(kismetData);
+                // Split kismet data by white space
+                String[] pkt = kismetData.split("\\s+");
+
+                // Grab BSSID
+                if("*BSSID:".equals(pkt[0])) {
+                    // if channel is not equal to zero and packet is added to set
+                    if ("0".equals(pkt[3]) && !("0".equals(pkt[2])) && networksSeen.add(pkt[1])) {
+                        // Add network to seen list
+                        this.setNetworkCount(networksSeen.size());
+                        System.out.println(networksSeen);
+                    }
+                }
+                
                 // do something with kismet message
             }
         } catch (Exception ex) {
             System.err.println(ex);
         }
     }
+    
+    public int getNetworkCount() { return this.networkCount; }
+    public void setNetworkCount(int count) { this.networkCount+=1; }
 }
