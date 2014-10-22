9008889a92d5b854b9807d5ceb8a262a8bd5a10b
rabbit <rabbit@ubuntu>
2014-10-22 12:33:10 -0400

    cleaned up code

--- src/com/blackgatetech/wardriver/lib/KismetClient.java
+++ src/com/blackgatetech/wardriver/lib/KismetClient.java
@@ -10,6 +10,7 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class KismetClient implements Runnable {
+    // Declare variables used throughout connection
     final private String host;
     final private int port;
     private BufferedReader fromServer;
@@ -28,19 +29,32 @@ public class KismetClient implements Runnable {
 
     public boolean connectToServer() throws ConnectException, UnknownHostException, IOException {
         try {
+            // Open a socket connection
             socket = new Socket(this.host, this.port);
+            
+            // Create BufferReader/Writer for talking with Kismet Server
             fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             toServer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            
+            // Announce connection to console
             System.out.println("Connected to " + this.host + ":" + this.port);
-            System.out.println("Subscribing to BSSID headers");
+            
+            // Send Kismet commands 
             toServer.write("!0 REMOVE TIME\r\n");
             toServer.write("!0 ENABLE BSSID bssid,channel\r\n");
             toServer.write("!0 ENABLE SSID mac,ssid\r\n");
+            
+            // Flush the output stream
             toServer.flush();
+            
+            // Begin a new thread
             Thread thread = new Thread(this);
             thread.start();
+            
+            // Respond with valid connection
             return true;
         } catch (ConnectException ex) {
+            // Respond with invalid connection
             System.err.println("Cannot connect to kismet server, it is probably down.");
             return false;
         }
@@ -48,6 +62,7 @@ public class KismetClient implements Runnable {
     
     public void disconnectFromServer() {
         try {
+            // Close the socket connection
             socket.close();
             System.out.println("You disconnected from the local server.");
         } catch (Exception ex) {
